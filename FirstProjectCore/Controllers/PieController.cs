27d058e9905c95846cf61cf0c155cb76e824eb40
AhmadIrfanMST <112946583+AhmadIrfanMST@users.noreply.github.com>
2023-05-09 18:11:11 +0500

    initial

---
+++ FirstProjectCore/Controllers/PieController.cs
@@ -0,0 +1,23 @@
+﻿using FirstProjectCore.Models;
+using FirstProjectCore.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstProjectCore.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+        public PieController(IPieRepository pie, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pie;
+            _categoryRepository = categoryRepository;   
+        }
+
+        public IActionResult List()
+        {
+            PieListViewModel piesListViewModel = new PieListViewModel(_pieRepository.AllPies,"Cheese cakes");
+            return View(piesListViewModel);
+        }
+    }
+}
